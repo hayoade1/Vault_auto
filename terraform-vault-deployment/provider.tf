8ebda80dd02f294d14d3d372db23d3f2f915b913
Sam Gabrail <sam.gabrail@tekanaid.com>
2020-03-27 16:37:16 -0400

    don't load config file

--- terraform-vault-deployment/provider.tf
+++ terraform-vault-deployment/provider.tf
@@ -14,6 +14,7 @@ provider "google" {
 }
 
 provider "kubernetes" {
+  load_config_file = "false"
   host  = google_container_cluster.kubernetes_cluster.endpoint
   token = data.google_client_config.default.access_token
 
