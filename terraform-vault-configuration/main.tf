444bb21e5a5833f56921fd179ab9b3ca803e6931
samgabrail <sameh.gabrail@gmail.com>
2020-11-13 10:30:23 -0500

    added azurerm creds

--- terraform-vault-configuration/main.tf
+++ terraform-vault-configuration/main.tf
@@ -12,7 +12,10 @@ terraform {
 }
 
 provider "azurerm" {
-  # Configuration options
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
   features {}
 }
 
