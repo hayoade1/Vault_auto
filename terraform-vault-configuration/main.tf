8e822cffdf4cf0ad8a9427d66d8ba7b39309d7eb
samgabrail <sameh.gabrail@gmail.com>
2020-12-01 11:00:32 -0500

    added jenkins policies

--- terraform-vault-configuration/main.tf
+++ terraform-vault-configuration/main.tf
@@ -226,3 +226,46 @@ resource "vault_azure_secret_backend_role" "jenkins" {
     scope =  "/subscriptions/${var.subscription_id}/resourceGroups/${azurerm_resource_group.myresourcegroup.name}"
   }
 }
+
+# Jenkins Secure Introduction
+
+resource "vault_mount" "hashitalks_kv" {
+  path = "kv"
+  type = "generic"
+}
+
+resource "vault_generic_secret" "demo_secrets" {
+  path = "kv/pipeline-secrets"
+
+  data_json = <<EOT
+{
+  "secret-1": "i_am_a_secret",
+  "secret-2": "give_me_the_piece_of_cake"
+}
+EOT
+ depends_on = [vault_mount.hashitalks_kv]
+}
+
+resource "vault_policy" "pipeline_policy" {
+  name = "pipeline-policy"
+  policy = file("policies/jenkins_pipeline_policy.hcl")
+}
+
+resource "vault_policy" "trusted_entity_policy" {
+  name = "trusted-entity"
+  policy = file("policies/jenkins_policy.hcl")
+}
+
+resource "vault_auth_backend" "pipeline_access" {
+  type = "approle"
+  path = "pipeline"
+}
+
+resource "vault_approle_auth_backend_role" "pipeline_approle" {
+  backend            = vault_auth_backend.pipeline_access.path
+  role_name          = "pipeline-approle"
+  secret_id_num_uses = "1"
+  secret_id_ttl      = "300"
+  token_ttl          = "1800"
+  token_policies     = ["default", "pipeline-policy"]
+}
---
+++ terraform-vault-configuration/policies/jenkins_pipeline_policy.hcl
@@ -0,0 +1,7 @@
+path "kv/data/pipeline-secrets" {
+  policy = "read"
+}
+
+path "kv/pipeline-secrets" {
+  policy = "read"
+}
