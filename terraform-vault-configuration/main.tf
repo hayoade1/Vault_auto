1e21b965a81897c06ae86dbd23d6ed0d4394b601
samgabrail <sameh.gabrail@gmail.com>
2021-02-22 16:03:31 -0500

    added ado policy and azure creds

--- terraform-vault-configuration/main.tf
+++ terraform-vault-configuration/main.tf
@@ -42,6 +42,11 @@ resource "vault_policy" "operations_policy" {
   policy = file("policies/operation_policy.hcl")
 }
 
+resource "vault_policy" "ado_policy" {
+  name   = "ado_policy"
+  policy = file("policies/ado_policy.hcl")
+}
+
 resource "vault_mount" "developers" {
   path        = "developers"
   type        = "kv-v2"
@@ -110,6 +115,19 @@ resource "vault_generic_secret" "webblog" {
 {
   "username": "${var.DB_USER}",
   "password": "${var.DB_PASSWORD}"
+}
+EOT
+}
+
+resource "vault_generic_secret" "azure" {
+  path = "${vault_mount.internal.path}/azure"
+
+  data_json = <<EOT
+{
+  "subscription_id": "${var.subscription_id}",
+  "tenant_id": "${var.tenant_id}",
+  "client_secret": "${var.client_secret}",
+  "client_id": "${var.client_id}"
 }
 EOT
 }
